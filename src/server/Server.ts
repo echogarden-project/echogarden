420a3e65e97516d1677d034b8dce1fd2650c7b2c
Rotem Dan <rotemdan@gmail.com>
2023-04-28 19:28:01 +0300

    Some initial work on server

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,23 +1,31 @@
-import { WebSocketServer } from 'ws'
+import { WebSocketServer, RawData, WebSocket } from 'ws'
 import { encode as encodeMsgPack, decode as decodeMsgPack } from 'msgpack-lite'
 import { getRandomHexString } from '../utilities/Utilities.js'
 
-async function start() {
+export async function startServer() {
 	const wss = new WebSocketServer({ port: 8080 })
 
 	wss.on('connection', async (ws) => {
 		const connectionId = getRandomHexString(32)
 
-		ws.on('message', async (data) => {
-			const message = {
-				someData: 'something'
-			}
-
-			ws.send(encodeMsgPack(message))
+		ws.on('message', (data) => {
+			handleMessage(connectionId, data, ws)
 		})
 
-		ws.on('error', console.error)
+		ws.on('error', (e) => {
+			throw e
+		})
 	})
 }
 
-start()
+async function handleMessage(connectionId: string, data: RawData, ws: WebSocket) {
+	const incomingMessage = decodeMsgPack(data as Buffer)
+
+	const outgoingMessage = {
+		someData: 'something'
+	}
+
+	const encodedOutgoingMessage = encodeMsgPack(outgoingMessage)
+
+	ws.send(encodedOutgoingMessage)
+}
