13f597243ff04af0475bea2c3175773ece632779
Rotem Dan <rotemdan@gmail.com>
2023-05-03 08:42:17 +0300

    Update package.json, add basic server starter script

---
+++ src/server/ServerStarter.ts
@@ -0,0 +1,13 @@
+import { logToStderr, setupProgramTerminationListeners, setupUnhandledExceptionListeners } from '../utilities/Utilities.js'
+import { startWebSocketServer } from './Server.js'
+
+const log = logToStderr
+
+setupUnhandledExceptionListeners()
+
+setupProgramTerminationListeners(() => {
+	log('')
+})
+
+await startWebSocketServer({}, (options) => {
+})
