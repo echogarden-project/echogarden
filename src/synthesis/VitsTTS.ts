d4a9f45b0ddf4b80eeba6ec08cfcd405373fd814
Rotem Dan <rotemdan@gmail.com>
2023-07-19 08:10:24 +0300

    Extend VITS voice list. Make model file loading more robust

--- src/synthesis/VitsTTS.ts
+++ src/synthesis/VitsTTS.ts
@@ -4,7 +4,8 @@ import { Logger } from "../utilities/Logger.js"
 import { RawAudio, getEmptyRawAudio } from "../audio/AudioUtilities.js"
 import { Lexicon } from "../nlp/Lexicon.js"
 import { Timeline } from "../utilities/Timeline.js"
-import { readAndParseJsonFile } from "../utilities/FileSystem.js"
+import { readAndParseJsonFile, readdir } from "../utilities/FileSystem.js"
+import path from 'node:path'
 
 const cachedInstanceLookup = new Map<string, VitsTTS>()
 
@@ -49,8 +50,17 @@ export class VitsTTS {
 
 		const { default: Onnx } = await import('onnxruntime-node')
 
-		this.modelSession = await Onnx.InferenceSession.create(`${this.modelPath}/${this.voiceName}.onnx`, onnxOptions)
-		this.metadata = await readAndParseJsonFile(`${this.modelPath}/${this.voiceName}.onnx.json`)
+		const filesInModelPath = await readdir(this.modelPath)
+		const onnxModelFilename = filesInModelPath.find(filename => filename.endsWith(".onnx"))
+
+		if (!onnxModelFilename) {
+			throw new Error(`Couldn't file any ONNX model file in ${this.modelPath}`)
+		}
+
+		const onnxModelFilepath = path.join(this.modelPath, onnxModelFilename)
+
+		this.modelSession = await Onnx.InferenceSession.create(onnxModelFilepath, onnxOptions)
+		this.metadata = await readAndParseJsonFile(`${onnxModelFilepath}.json`)
 
 		this.phonemeMap = new Map<string, number[]>()
 
@@ -74,6 +84,9 @@ export class VitsTTS {
 		const phonemeMap = this.phonemeMap!
 		const espeakVoice = metadata.espeak.voice
 		const outputSampleRate = metadata.audio.sample_rate
+		const baseLengthScale = metadata.inference.length_scale || 1.0
+
+		lengthScale *= baseLengthScale
 
 		sentence = //simplifyPunctuationCharacters(sentence.trim())
 			sentence
@@ -191,294 +204,453 @@ export class VitsTTS {
 
 export const voiceList: SynthesisVoice[] = [
 	{
-		name: "ca-upc_ona-x-low",
+		name: "ca_ES-upc_ona-x_low",
 		languages: ["ca-ES", "ca"],
 		gender: "female",
 	},
 	{
-		name: "ca-upc_pau-x-low",
+		name: "ca_ES-upc_ona-medium",
+		languages: ["ca-ES", "ca"],
+		gender: "female",
+	},
+	{
+		name: "ca_ES-upc_pau-x_low",
 		languages: ["ca-ES", "ca"],
 		gender: "male",
 	},
 
 	{
-		name: "da-nst_talesyntese-medium",
+		name: "da_DK-nst_talesyntese-medium",
 		languages: ["da-DK", "da"],
 		gender: "male",
 	},
 
 	{
-		name: "de-thorsten-low",
+		name: "de_DE-thorsten-low",
 		languages: ["de-DE", "de"],
 		gender: "male",
 	},
 	{
-		name: "de-eva_k-x-low",
-		languages: ["de-DE", "de"],
-		gender: "female",
-	},
-	{
-		name: "de-ramona-low",
-		languages: ["de-DE", "de"],
-		gender: "female",
-	},
-	{
-		name: "de-pavoque-low",
+		name: "de_DE-thorsten-medium",
 		languages: ["de-DE", "de"],
 		gender: "male",
 	},
 	{
-		name: "de-kerstin-low",
+		name: "de_DE-eva_k-x_low",
 		languages: ["de-DE", "de"],
 		gender: "female",
 	},
 	{
-		name: "de-karlsson-low",
+		name: "de_DE-ramona-low",
+		languages: ["de-DE", "de"],
+		gender: "female",
+	},
+	{
+		name: "de_DE-pavoque-low",
+		languages: ["de-DE", "de"],
+		gender: "male",
+	},
+	{
+		name: "de_DE-kerstin-low",
+		languages: ["de-DE", "de"],
+		gender: "female",
+	},
+	{
+		name: "de_DE-karlsson-low",
 		languages: ["de-DE", "de"],
 		gender: "male",
 	},
 
 	{
-		name: "el-gr-rapunzelina-low",
+		name: "el_GR-rapunzelina-low",
 		languages: ["el-GR", "el"],
 		gender: "female",
 	},
 
-
 	{
-		name: "en-gb-danny-low",
+		name: "en_GB-danny-low",
 		languages: ["en-GB", "en"],
 		gender: "male",
 	},
 	{
-		name: "en-gb-alan-low",
+		name: "en_GB-alan-low",
 		languages: ["en-GB", "en"],
 		gender: "male",
 	},
 	{
-		name: "en-gb-southern_english_female-low",
+		name: "en_GB-alan-medium",
+		languages: ["en-GB", "en"],
+		gender: "male",
+	},
+	{
+		name: "en_GB-alba-medium",
+		languages: ["en-GB", "en"],
+		gender: "female",
+	},
+	{
+		name: "en_GB-aru-medium",
+		languages: ["en-GB", "en"],
+		gender: "female",
+		speakerCount: 12,
+	},
+	{
+		name: "en_GB-southern_english_female-low",
+		languages: ["en-GB", "en"],
+		gender: "female",
+	},
+	{
+		name: "en_GB-northern_english_male-medium",
+		languages: ["en-GB", "en"],
+		gender: "male",
+	},
+	{
+		name: "en_GB-vctk-medium",
+		languages: ["en-GB", "en"],
+		gender: "unknown",
+		speakerCount: 109,
+	},
+	{
+		name: "en_GB-jenny_dioco-medium",
 		languages: ["en-GB", "en"],
 		gender: "female",
 	},
 
 	{
-		name: "en-us-amy-low",
+		name: "en_US-amy-low",
 		languages: ["en-US", "en"],
 		gender: "female",
 	},
 	{
-		name: "en-us-kathleen-low",
+		name: "en_US-amy-medium",
 		languages: ["en-US", "en"],
 		gender: "female",
 	},
 	{
-		name: "en-us-lessac-low",
+		name: "en_US-kathleen-low",
 		languages: ["en-US", "en"],
 		gender: "female",
 	},
 	{
-		name: "en-us-lessac-medium",
+		name: "en_US-lessac-low",
 		languages: ["en-US", "en"],
 		gender: "female",
 	},
 	{
-		name: "en-us-libritts-high",
+		name: "en_US-lessac-medium",
+		languages: ["en-US", "en"],
+		gender: "female",
+	},
+	{
+		name: "en_US-lessac-high",
+		languages: ["en-US", "en"],
+		gender: "female",
+	},
+	{
+		name: "en_US-libritts-high",
 		languages: ["en-US", "en"],
 		gender: "unknown",
 		speakerCount: 904,
 	},
 	{
-		name: "en-us-ryan-low",
+		name: "en_US-ryan-low",
 		languages: ["en-US", "en"],
 		gender: "male",
 	},
 	{
-		name: "en-us-ryan-medium",
+		name: "en_US-ryan-medium",
 		languages: ["en-US", "en"],
 		gender: "male",
 	},
 	{
-		name: "en-us-ryan-high",
+		name: "en_US-ryan-high",
 		languages: ["en-US", "en"],
+		gender: "male",
+	},
+	{
+		name: "en_US-joe-medium",
+		languages: ["en-US", "en"],
+		gender: "male",
+	},
+	{
+		name: "en_US-kusal-medium",
+		languages: ["en-US", "en"],
+		gender: "male",
+	},
+	{
+		name: "en_US-arctic-medium",
+		languages: ["en-US", "en"],
+		gender: "male",
+		speakerCount: 18
+	},
+	{
+		name: "en_US-l2arctic-medium",
+		languages: ["en-US", "en"],
+		gender: "male",
+		speakerCount: 24
+	},
+
+	{
+		name: "es_ES-carlfm-x_low",
+		languages: ["es-ES", "es"],
+		gender: "male",
+	},
+	{
+		name: "es_ES-sharvard-medium",
+		languages: ["es-ES", "es"],
+		gender: "male",
+		speakerCount: 2
+	},
+	{
+		name: "es_ES-davefx-medium",
+		languages: ["es-ES", "es"],
+		gender: "male",
+	},
+	{
+		name: "es_ES-mls_9972-low",
+		languages: ["es-ES", "es"],
+		gender: "male",
+	},
+	{
+		name: "es_ES-mls_10246-low",
+		languages: ["es-ES", "es"],
 		gender: "male",
 	},
 
 	{
-		name: "es-carlfm-x-low",
-		languages: ["es-ES", "es"],
-		gender: "male",
-	},
-	{
-		name: "es-mls_9972-low",
-		languages: ["es-ES", "es"],
-		gender: "male",
-	},
-	{
-		name: "es-mls_10246-low",
-		languages: ["es-ES", "es"],
+		name: "es_MX-ald-medium",
+		languages: ["es-MX", "es"],
 		gender: "male",
 	},
 
 	{
-		name: "fi-harri-low",
+		name: "fi_FI-harri-low",
+		languages: ["fi-FI", "fi"],
+		gender: "female",
+	},
+	{
+		name: "fi_FI-harri-medium",
 		languages: ["fi-FI", "fi"],
 		gender: "female",
 	},
 
 	{
-		name: "fr-siwis-low",
+		name: "fr_FR-siwis-low",
 		languages: ["fr-FR", "fr"],
 		gender: "female",
 	},
 	{
-		name: "fr-siwis-medium",
+		name: "fr_FR-siwis-medium",
 		languages: ["fr-FR", "fr"],
 		gender: "female",
 	},
 	{
-		name: "fr-mls_1840-low",
+		name: "fr_FR-mls_1840-low",
+		languages: ["fr-FR", "fr"],
+		gender: "male",
+	},
+	{
+		name: "fr_FR-gilles-low",
 		languages: ["fr-FR", "fr"],
 		gender: "male",
 	},
 
 	{
-		name: "is-ugla-medium",
+		name: "is_IS-ugla-medium",
 		languages: ["is-IS", "is"],
 		gender: "female",
 	},
 	{
-		name: "is-steinn-medium",
+		name: "is_IS-steinn-medium",
 		languages: ["is-IS", "is"],
 		gender: "male",
 	},
 	{
-		name: "is-salka-medium",
+		name: "is_IS-salka-medium",
 		languages: ["is-IS", "is"],
 		gender: "female",
 	},
 	{
-		name: "is-bui-medium",
+		name: "is_IS-bui-medium",
 		languages: ["is-IS", "is"],
 		gender: "male",
 	},
 
 	{
-		name: "it-riccardo_fasol-x-low",
+		name: "it_IT-riccardo-x_low",
 		languages: ["it-IT", "it"],
 		gender: "male",
 	},
 
 	{
-		name: "kk-iseke-x-low",
+		name: "ka_GE-natia-medium",
+		languages: ["ka-GE", "ka"],
+		gender: "female",
+	},
+
+	{
+		name: "kk_KZ-iseke-x_low",
+		languages: ["kk-KZ", "kk"],
+		gender: "male",
+	},
+	{
+		name: "kk_KZ-raya-x_low",
+		languages: ["kk-KZ", "kk"],
+		gender: "male",
+	},
+	{
+		name: "kk_KZ-issai-high",
 		languages: ["kk-KZ", "kk"],
 		gender: "unknown",
 		speakerCount: 6,
 	},
-	{
-		name: "kk-raya-x-low",
-		languages: ["kk-KZ", "kk"],
-		gender: "male",
-	},
-	{
-		name: "kk-issai-high",
-		languages: ["kk-KZ", "kk"],
-		gender: "male",
-	},
 
 	{
-		name: "ne-google-medium",
+		name: "ne_NP-google-medium",
 		languages: ["ne-NP", "ne"],
 		gender: "female",
 		speakerCount: 18,
 	},
 	{
-		name: "ne-google-x-low",
+		name: "ne_NP-google-x_low",
 		languages: ["ne-NP", "ne"],
 		gender: "female",
 		speakerCount: 18,
 	},
 
 	{
-		name: "nl-nathalie-x-low",
+		name: "nl_NL-mls_5809-low",
 		languages: ["nl-NL", "nl"],
 		gender: "female",
 	},
 	{
-		name: "nl-rdh-medium",
-		languages: ["nl-NL", "nl"],
-		gender: "male",
-	},
-	{
-		name: "nl-rdh-x-low",
-		languages: ["nl-NL", "nl"],
-		gender: "male",
-	},
-	{
-		name: "nl-mls_5809-low",
-		languages: ["nl-NL", "nl"],
-		gender: "female",
-	},
-	{
-		name: "nl-mls_7432-low",
+		name: "nl_NL-mls_7432-low",
 		languages: ["nl-NL", "nl"],
 		gender: "female",
 	},
 
 	{
-		name: "no-talesyntese-medium",
+		name: "nl_BE-nathalie-x_low",
+		languages: ["nl-BE", "nl"],
+		gender: "female",
+	},
+	{
+		name: "nl_BE-nathalie-medium",
+		languages: ["nl-BE", "nl"],
+		gender: "female",
+	},
+	{
+		name: "nl_BE-rdh-medium",
+		languages: ["nl-BE", "nl"],
+		gender: "male",
+	},
+	{
+		name: "nl_BE-rdh-x_low",
+		languages: ["nl-BE", "nl"],
+		gender: "male",
+	},
+
+
+	{
+		name: "no_NO-talesyntese-medium",
 		languages: ["no-NO", "no"],
 		gender: "male",
 	},
 
 	{
-		name: "pl-mls_6892-low",
+		name: "pl_PL-mls_6892-low",
 		languages: ["pl-PL", "pl"],
 		gender: "male",
 	},
+	{
+		name: "pl_PL-darkman-medium",
+		languages: ["pl-PL", "pl"],
+		gender: "male",
+	},
+	{
+		name: "pl_PL-gosia-medium",
+		languages: ["pl-PL", "pl"],
+		gender: "female",
+	},
 
 	{
-		name: "pt-br-edresson-low",
+		name: "pt_BR-edresson-low",
+		languages: ["pt-BR", "pt"],
+		gender: "male",
+	},
+	{
+		name: "pt_BR-faber-medium",
 		languages: ["pt-BR", "pt"],
 		gender: "male",
 	},
 
 	{
-		name: "ru-irinia-medium",
+		name: "ru_RU-ruslan-medium",
+		languages: ["ru-RU", "ru"],
+		gender: "male",
+	},
+	{
+		name: "ru_RU-irinia-medium",
 		languages: ["ru-RU", "ru"],
 		gender: "female",
 	},
+	{
+		name: "ru_RU-denis-medium",
+		languages: ["ru-RU", "ru"],
+		gender: "male",
+	},
+	{
+		name: "ru_RU-dmitri-medium",
+		languages: ["ru-RU", "ru"],
+		gender: "male",
+	},
 
 	{
-		name: "sv-se-nst-medium",
+		name: "sv_SE-nst-medium",
 		languages: ["sv-SE", "sv"],
 		gender: "male",
 	},
 
 	{
-		name: "uk-lada-x-low",
-		languages: ["uk-UA", "uk"],
-		gender: "female",
+		name: "sw_CD-lanfrica-medium",
+		languages: ["sw-CD", "sw"],
+		gender: "male",
 	},
 
 	{
-		name: "vi-vivos-x-low",
+		name: "uk_UA-lada-x_low",
+		languages: ["uk-UA", "uk"],
+		gender: "female",
+	},
+	{
+		name: "uk_UA-ukrainian_tts-medium",
+		languages: ["uk-UA", "uk"],
+		gender: "female",
+		speakerCount: 3,
+	},
+	{
+		name: "vi_VN-vivos-x_low",
 		languages: ["vi-VN", "vi"],
 		gender: "unknown",
 		speakerCount: 65,
 	},
 	{
-		name: "vi-25hours-single-low",
+		name: "vi_VN-25hours-single-low",
+		languages: ["vi-VN", "vi"],
+		gender: "female",
+	},
+	{
+		name: "vi_VN-vais1000-medium",
 		languages: ["vi-VN", "vi"],
 		gender: "female",
 	},
 
 	{
-		name: "zh-cn-huayan-x-low",
+		name: "zh_CN-huayan-x_low",
 		languages: ["zh-CN", "zh"],
 		gender: "female",
 	},
 	{
-		name: "zh-cn-huayan-medium",
+		name: "zh_CN-huayan-medium",
 		languages: ["zh-CN", "zh"],
 		gender: "female",
 	},
