1268467dd787ce99582ad30b9e6c02e8df789682
Rotem Dan <rotemdan@gmail.com>
2023-07-19 08:11:48 +0300

    Move to new package system supporting version tags, and hosted in a  Hugging Face repository

--- src/utilities/PackageManager.ts
+++ src/utilities/PackageManager.ts
@@ -4,19 +4,9 @@ import { getAppDataDir, getAppTempDir, ensureDir, existsSync, remove } from "./F
 import { appName } from "../api/Globals.js"
 import { GaxiosOptions } from "gaxios"
 
-export async function getPackagePathIfExists(packageName: string) {
-	const packagesPath = await ensureAndGetPackagesDir()
-
-	const packagePath = path.join(packagesPath, packageName)
-
-	if (existsSync(packagePath)) {
-		return packagePath
-	}
-
-	return undefined
-}
-
 export async function loadPackage(packageName: string) {
+	packageName = resolveToVersionedPackageNameIfNeeded(packageName)
+
 	const packagesPath = await ensureAndGetPackagesDir()
 
 	const packagePath = path.join(packagesPath, packageName)
@@ -27,19 +17,11 @@ export async function loadPackage(packageName: string) {
 
 	const tempPath = getAppTempDir(appName)
 
-	let baseUrl: string
-
-	if (packageName.startsWith("whisper-")) {
-		baseUrl = "https://github.com/echogarden-project/whisper-onnx-models/releases/download/v0.0.1/"
-	} else {
-		baseUrl = "https://github.com/echogarden-project/echogarden-packages/releases/download/v0.0.1/"
-	}
-
 	const headers = {
 	}
 
 	const options: GaxiosOptions = {
-		url: `${baseUrl}${packageName}.tar.gz`,
+		url: `${basePackageUrl}${packageName}.tar.gz`,
 		headers
 	}
 
@@ -54,6 +36,8 @@ export async function loadPackage(packageName: string) {
 }
 
 export async function removePackage(packageName: string) {
+	packageName = resolveToVersionedPackageNameIfNeeded(packageName)
+
 	const packagesPath = await ensureAndGetPackagesDir()
 
 	const packagePath = path.join(packagesPath, packageName)
@@ -69,4 +53,32 @@ export async function ensureAndGetPackagesDir() {
 	await ensureDir(packagesPath)
 
 	return packagesPath
+}
+
+export function resolveToVersionedPackageNameIfNeeded(packageName: string) {
+	const versionTag = getVersionTagFromPackageName(packageName)
+
+	if (versionTag) {
+		return packageName
+	}
+
+	const resolvedVersionTag = resolveVersionTagForUnversionedPackageName(packageName)
+
+	return packageName = `${packageName}-${resolvedVersionTag}`
+}
+
+export function getVersionTagFromPackageName(packageName: string) {
+	return packageName.match(/.*\-([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9](_[0-9]+)?)$/)?.[1]
+}
+
+export function resolveVersionTagForUnversionedPackageName(unversionedPackageName: string) {
+	return packageVersionTagResolutionLookup[unversionedPackageName] || defaultVersionTag
+}
+
+const basePackageUrl = "https://huggingface.co/echogarden/echogarden-packages/resolve/main/"
+
+const defaultVersionTag = "20230718"
+
+const packageVersionTagResolutionLookup: { [packageName: string]: string } = {
+
 }
