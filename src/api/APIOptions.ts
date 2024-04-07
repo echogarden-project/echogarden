da286e0cf2aa075371a6c1eef137700a9060b406
Rotem Dan <rotemdan@gmail.com>
2024-04-07 06:04:21 +0300

    Code cleanup.

--- src/api/APIOptions.ts
+++ src/api/APIOptions.ts
@@ -1,15 +1,16 @@
-import * as API from "./API.js"
-import type { ServerOptions } from "../server/Server.js"
+import * as API from './API.js'
+import type { ServerOptions } from '../server/Server.js'
 
-export type APIOptions = {
-	VoiceListRequestOptions: API.VoiceListRequestOptions,
-	SynthesisOptions: API.SynthesisOptions,
-	RecognitionOptions: API.RecognitionOptions,
-	AlignmentOptions: API.AlignmentOptions,
+export interface APIOptions {
+	VoiceListRequestOptions: API.VoiceListRequestOptions
+	SynthesisOptions: API.SynthesisOptions
+	RecognitionOptions: API.RecognitionOptions
+	AlignmentOptions: API.AlignmentOptions
 	SpeechTranslationOptions: API.SpeechTranslationOptions
-	SpeechLanguageDetectionOptions: API.SpeechLanguageDetectionOptions,
-	TextLanguageDetectionOptions: API.TextLanguageDetectionOptions,
-	VADOptions: API.VADOptions,
-	DenoisingOptions: API.DenoisingOptions,
+	SpeechLanguageDetectionOptions: API.SpeechLanguageDetectionOptions
+	TextLanguageDetectionOptions: API.TextLanguageDetectionOptions
+	VADOptions: API.VADOptions
+	DenoisingOptions: API.DenoisingOptions
+	SourceSeparationOptions: API.SourceSeparationOptions
 	ServerOptions: ServerOptions
 }
