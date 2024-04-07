0611ff1b22adc4474a4dbe3236bb15c0b9e17a5e
Rotem Dan <rotemdan@gmail.com>
2024-04-07 05:52:45 +0300

    Include multilingual voices languages in Elevenlabs. Code cleanup.

--- src/synthesis/ElevenLabsTTS.ts
+++ src/synthesis/ElevenLabsTTS.ts
@@ -1,14 +1,17 @@
-import { GaxiosResponse, request } from "gaxios"
-import { SynthesisVoice, VoiceGender } from "../api/API.js"
-import * as FFMpegTranscoder from "../codecs/FFMpegTranscoder.js"
-import { Logger } from "../utilities/Logger.js"
-import { logToStderr } from "../utilities/Utilities.js"
+import { GaxiosResponse, request } from 'gaxios'
+import { SynthesisVoice, VoiceGender } from '../api/API.js'
+import * as FFMpegTranscoder from '../codecs/FFMpegTranscoder.js'
+import { Logger } from '../utilities/Logger.js'
+import { logToStderr } from '../utilities/Utilities.js'
+import { extendDeep } from '../utilities/ObjectUtilities.js'
 
 const log = logToStderr
 
-export async function synthesize(text: string, voiceId: string, apiKey: string, modelId: string, stability = 0, similarityBoost = 0) {
+export async function synthesize(text: string, voiceId: string, modelId: string, options: ElevenlabsTTSOptions) {
 	const logger = new Logger()
-	logger.start("Request synthesis from ElevenLabs")
+	logger.start('Request synthesis from ElevenLabs')
+
+	options = extendDeep(defaultElevenlabsTTSOptions, options)
 
 	let response: GaxiosResponse<any>
 
@@ -16,11 +19,11 @@ export async function synthesize(text: string, voiceId: string, apiKey: string, 
 		response = await request<any>({
 			url: `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`,
 
-			method: "POST",
+			method: 'POST',
 
 			headers: {
-				"accept": "audio/mpeg",
-				"xi-api-key": apiKey,
+				'accept': 'audio/mpeg',
+				'xi-api-key': options.apiKey,
 			},
 
 			data: {
@@ -29,12 +32,14 @@ export async function synthesize(text: string, voiceId: string, apiKey: string, 
 				model_id: modelId,
 
 				voice_setting: {
-					stability,
-					similarity_boost: similarityBoost
+					stability: options.stability,
+					similarity_boost: options.similarityBoost,
+					style: options.style,
+					use_speaker_boost: options.useSpeakerBoost
 				}
 			},
 
-			responseType: "arraybuffer"
+			responseType: 'arraybuffer'
 		})
 	} catch (e: any) {
 		const response = e.response
@@ -51,7 +56,7 @@ export async function synthesize(text: string, voiceId: string, apiKey: string, 
 		throw e
 	}
 
-	logger.start("Decode synthesized audio")
+	logger.start('Decode synthesized audio')
 	const rawAudio = await FFMpegTranscoder.decodeToChannels(Buffer.from(response.data))
 
 	logger.end()
@@ -61,44 +66,72 @@ export async function synthesize(text: string, voiceId: string, apiKey: string, 
 
 export async function getVoiceList(apiKey: string) {
 	const response = await request<any>({
-		method: "GET",
+		method: 'GET',
 
-		url: "https://api.elevenlabs.io/v1/voices",
+		url: 'https://api.elevenlabs.io/v1/voices',
 
 		headers: {
-			"accept": "accept: application/json",
-			"xi-api-key": apiKey
+			'accept': 'accept: application/json',
+			'xi-api-key': apiKey
 		},
 
-		responseType: "json"
+		responseType: 'json'
 	})
 
-	const elevenLabsVoices: any[] = response.data.voices
+	const elevenlabsVoices: any[] = response.data.voices
 
-	const voices: SynthesisVoice[] = elevenLabsVoices.map(elevenLabsVoice => {
-		const accent: string | undefined = elevenLabsVoice?.labels?.accent
-		const gender: VoiceGender = elevenLabsVoice?.labels?.gender || "unknown"
+	const voices: SynthesisVoice[] = elevenlabsVoices.map(elevenlabsVoice => {
+		const modelId: string = elevenlabsVoice?.high_quality_base_model_ids?.[0] ?? 'eleven_monolingual_v1'
+		const accent: string | undefined = elevenlabsVoice?.labels?.accent
+		const gender: VoiceGender = elevenlabsVoice?.labels?.gender ?? 'unknown'
 
-		let language: string
+		const supportedLanguages: string[] = []
 
-		if (!accent || accent.startsWith("american")) {
-			language = 'en-US'
-		} else if (accent.startsWith("british") || accent == "irish") {
-			language = 'en-GB'
-		} else if (accent == "australian") {
-			language = 'en-AU'
+		if (accent) {
+			if (accent.startsWith('american')) {
+				supportedLanguages.push('en-US')
+			} else if (accent.startsWith('british')) {
+				supportedLanguages.push('en-GB')
+			} else if (accent === 'irish') {
+				supportedLanguages.push('en-IE')
+			} else if (accent == 'australian') {
+				supportedLanguages.push('en-AU')
+			}
+		}
+
+		if (modelId.includes('multilingual')) {
+			supportedLanguages.push('en', ...supporteMultilingualLanguages)
 		} else {
-			language = 'en-US'
+			supportedLanguages.push('en')
 		}
 
 		return {
-			name: elevenLabsVoice.name,
-			languages: [language, 'en'],
+			name: elevenlabsVoice.name,
+			languages: supportedLanguages,
 			gender,
 
-			elevenLabsVoiceId: elevenLabsVoice.voice_id,
-			elevenLabsModelId: elevenLabsVoice?.high_quality_base_model_ids?.[0] || 'eleven_monolingual_v1'
-		}})
+			elevenLabsVoiceId: elevenlabsVoice.voice_id,
+			elevenLabsModelId: modelId
+		}
+	})
 
 	return voices
 }
+
+export interface ElevenlabsTTSOptions {
+	apiKey?: string
+	stability?: number
+	similarityBoost?: number
+	style?: number
+	useSpeakerBoost?: boolean
+}
+
+export const defaultElevenlabsTTSOptions = {
+	apiKey: undefined,
+	stability: 0.5,
+	similarityBoost: 0.5,
+	style: 0,
+	useSpeakerBoost: true
+}
+
+export const supporteMultilingualLanguages = ['zh', 'ko', 'nl', 'tr', 'sv', 'id', 'tl', 'ja', 'uk', 'el', 'cs', 'fi', 'ro', 'ru', 'da', 'bg', 'ms', 'sk', 'hr', 'ar', 'ta', 'pl', 'de', 'es', 'fr', 'it', 'hi', 'pt']
