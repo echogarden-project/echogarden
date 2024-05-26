6f7534ede8e84bacc3522728fbde5f38f8b8461d
Rotem Dan <rotemdan@gmail.com>
2024-05-26 10:00:34 +0300

    Add initial module for text translation

---
+++ src/api/TextTranslation.ts
@@ -0,0 +1,9 @@
+export async function translateText(input: string, options: TextTranslationOptions): Promise<TextTranslationResult> {
+	return {}
+}
+
+export interface TextTranslationOptions {
+}
+
+export interface TextTranslationResult {
+}
