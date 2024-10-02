09f0513bf8a99ac3f779eb31ebc1a750a4582dc8
Rotem Dan <rotemdan@gmail.com>
2024-10-02 19:36:43 +0300

    Add VirtualFileReadStream utility class

---
+++ src/utilities/VirtualFileReadStream.ts
@@ -0,0 +1,109 @@
+import { Readable } from 'stream'
+import { EventEmitter } from 'events'
+import { ReadStream as FsReadStream } from 'fs'
+
+export function createVirtualFileReadStreamForBuffer(buffer: Buffer, path?: string | Buffer) {
+	const bufferReadStream = new VirtualFileReadStream(buffer, path)
+
+	const proxyStream = new Proxy<FsReadStream>(bufferReadStream, {
+		get(target, prop) {
+			if (prop === Symbol.toStringTag) {
+				return 'ReadStream'
+			}
+
+			return (target as any)[prop]
+		},
+
+		getPrototypeOf() {
+			return FsReadStream.prototype
+		}
+	})
+
+	return proxyStream
+}
+
+export class VirtualFileReadStream extends Readable {
+	private readonly buffer: Buffer
+
+	position: number
+	bytesRead: number
+	readonly path: string | Buffer
+	pending: boolean
+	readonly emitter: EventEmitter
+
+	constructor(buffer: Buffer, virtualPath?: string | Buffer) {
+		super({ autoDestroy: false })
+
+		this.buffer = buffer
+		
+		this.position = 0
+		this.bytesRead = 0
+		this.path = virtualPath || ''
+		this.pending = true
+		this.emitter = new EventEmitter()
+
+		this.emitter.emit('open', 0)
+
+		this.pending = false
+		this.emitter.emit('ready')
+	}
+
+	_read(size: number): void {
+		if (this.position >= this.buffer.length) {
+			this.push(null)
+
+			this.emitter.emit('end')
+			this.emitter.emit('finish')
+			this.emitter.emit('close')
+
+			return
+		}
+
+		const chunkSize = Math.min(size, this.buffer.length - this.position)
+
+		const chunk = this.buffer.subarray(this.position, this.position + chunkSize)
+
+		this.position += chunkSize
+		this.bytesRead = this.position
+
+		this.push(chunk)
+	}
+
+	close(callback?: (err?: NodeJS.ErrnoException | null) => void): void {
+		this.emitter.emit('close')
+
+		if (callback) {
+			callback(null)
+		}
+	}
+
+	addListener(event: string | symbol, listener: (...args: any[]) => void): this {
+		this.emitter.addListener(event, listener)
+
+		return this
+	}
+
+	on(event: string | symbol, listener: (...args: any[]) => void): this {
+		this.emitter.on(event, listener)
+
+		return this
+	}
+
+	once(event: string | symbol, listener: (...args: any[]) => void): this {
+		this.emitter.once(event, listener)
+
+		return this
+	}
+
+	prependListener(event: string | symbol, listener: (...args: any[]) => void): this {
+		this.emitter.prependListener(event, listener)
+
+		return this
+	}
+
+	prependOnceListener(event: string | symbol, listener: (...args: any[]) => void): this {
+		this.emitter.prependOnceListener(event, listener)
+
+		return this
+	}
+}
