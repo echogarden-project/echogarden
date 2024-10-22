303138d0e3d5f043b03fc2198626457682039a3e
Rotem Dan <rotemdan@gmail.com>
2024-10-22 11:55:19 +0300

    Allow to customize padding character and character mapping in Base64 encoder/decoder

--- src/encodings/Base64.ts
+++ src/encodings/Base64.ts
@@ -1,51 +1,88 @@
 import { StringBuilder } from '../utilities/StringBuilder.js';
 
-export function encodeBase64(inputBytes: Uint8Array, addPadding = true): string {
+export function encodeBase64(
+	inputBytes: Uint8Array,
+	paddingCharacter: string | undefined = '=',
+	charCodeMap?: Uint8Array): string {
+
 	if (!inputBytes || inputBytes.length == 0) {
 		return ''
 	}
 
-	const stringBuilder = new StringBuilder()
+	let paddingCharCode: number
 
-	for (let readOffset = 0; readOffset < inputBytes.length; readOffset += 3) {
-		if (readOffset <= length - 3) {
-			const uint24 = inputBytes[readOffset] << 16 | inputBytes[readOffset + 1] << 8 | inputBytes[readOffset + 2]
+	if (paddingCharacter == null) {
+		paddingCharCode = -1
+	} else if (paddingCharacter.length !== 1) {
+		throw new Error(`A padding character can only be a single character`)
+	} else {
+		paddingCharCode = paddingCharacter.charCodeAt(0)
+	}
 
-			stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 18) & 63])
-			stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 12) & 63])
-			stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 6) & 63])
-			stringBuilder.appendCharCode(charCodeMap[(uint24) & 63])
-		} else if (readOffset === length - 2) {
-			// If two bytes are left, output 3 encoded characters and one padding character
-			const uint24 = inputBytes[readOffset] << 16 | inputBytes[readOffset + 1] << 8
+	if (!charCodeMap) {
+		charCodeMap = defaultBase64CharCodeMap
+	}
 
-			stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 18) & 63])
-			stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 12) & 63])
-			stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 6) & 63])
+	const stringBuilder = new StringBuilder(inputBytes.length)
 
-			if (addPadding) {
-				stringBuilder.appendCharCode(paddingCharCode)
-			}
-		} else if (readOffset === length - 1) {
-			// Arrived at last byte at a position that did not complete a full 3 byte set
-			const uint24 = inputBytes[readOffset] << 16
+	const inputBytesLength = inputBytes.length
 
-			stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 18) & 63])
-			stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 12) & 63])
+	let readOffset = 0
 
-			if (addPadding) {
-				stringBuilder.appendCharCode(paddingCharCode)
-				stringBuilder.appendCharCode(paddingCharCode)
-			}
+	for (; readOffset <= inputBytesLength - 3; readOffset += 3) {
+		const uint24 =
+			inputBytes[readOffset] << 16 |
+			inputBytes[readOffset + 1] << 8 |
+			inputBytes[readOffset + 2]
+
+		stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 18) & 63])
+		stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 12) & 63])
+		stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 6) & 63])
+		stringBuilder.appendCharCode(charCodeMap[(uint24) & 63])
+	}
+
+	if (readOffset === inputBytesLength - 2) {
+		// If two bytes are left, output 3 encoded characters and one padding character
+		const uint24 =
+			inputBytes[readOffset] << 16 |
+			inputBytes[readOffset + 1] << 8
+
+		stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 18) & 63])
+		stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 12) & 63])
+		stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 6) & 63])
+
+		if (paddingCharCode >= 0) {
+			stringBuilder.appendCharCode(paddingCharCode)
+		}
+	} else if (readOffset === inputBytesLength - 1) {
+		// Arrived at last byte at a position that did not complete a full 3 byte set
+		const uint24 =
+			inputBytes[readOffset] << 16
+
+		stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 18) & 63])
+		stringBuilder.appendCharCode(charCodeMap[(uint24 >>> 12) & 63])
+
+		if (paddingCharCode >= 0) {
+			stringBuilder.appendCharCode(paddingCharCode)
+			stringBuilder.appendCharCode(paddingCharCode)
 		}
 	}
 
-	return stringBuilder.getOutputString()
+	return stringBuilder.toString()
 }
 
-export function decodeBase64(base64String: string, outputBuffer?: Uint8Array): Uint8Array {
+export function decodeBase64(
+	base64String: string,
+	outputBuffer?: Uint8Array,
+	paddingCharacter = '=',
+	reverseCharCodeMap?: Uint8Array): Uint8Array {
+
 	if (!base64String || base64String.length === 0) {
 		return new Uint8Array(0)
+	}
+
+	if (!paddingCharacter || paddingCharacter.length !== 1) {
+		throw new Error(`A single padding character must be provided for Base 64 decoding`)
 	}
 
 	// Add padding if omitted
@@ -60,8 +97,14 @@ export function decodeBase64(base64String: string, outputBuffer?: Uint8Array): U
 		base64String += paddingCharacter
 	}
 
+	if (!reverseCharCodeMap) {
+		reverseCharCodeMap = defaultBase64ReverseCharCodeMap
+	}
+
 	if (!outputBuffer) {
-		outputBuffer = new Uint8Array(base64String.length)
+		const capacity = (base64String.length / 4) * 3
+
+		outputBuffer = new Uint8Array(capacity)
 	}
 
 	const stringLength = base64String.length
@@ -81,19 +124,16 @@ export function decodeBase64(base64String: string, outputBuffer?: Uint8Array): U
 	}
 
 	// Remove 1 or 2 last bytes if padding characters were added to the string
-	if (base64String.charCodeAt(stringLength - 1) === paddingCharCode) {
+	if (base64String[stringLength - 1] === paddingCharacter) {
 		writeOffset--
 	}
 
-	if (base64String.charCodeAt(stringLength - 2) === paddingCharCode) {
+	if (base64String[stringLength - 2] === paddingCharacter) {
 		writeOffset--
 	}
 
 	return outputBuffer.subarray(0, writeOffset)
 }
 
-const charCodeMap: Uint8Array = new Uint8Array([65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 43, 47])
-const reverseCharCodeMap: Uint8Array = new Uint8Array([255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 62, 255, 255, 255, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 255, 255, 255, 0, 255, 255, 255, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 255, 255, 255, 255, 255, 255, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 255, 255, 255, 255])
-
-const paddingCharacter = '='
-const paddingCharCode = 61
+export const defaultBase64CharCodeMap: Uint8Array = new Uint8Array([65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 43, 47])
+export const defaultBase64ReverseCharCodeMap: Uint8Array = new Uint8Array([255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 62, 255, 255, 255, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 255, 255, 255, 0, 255, 255, 255, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 255, 255, 255, 255, 255, 255, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 255, 255, 255, 255])
