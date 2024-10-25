91773ae96872547620d73b0791c3b8ea6b728d3e
Rotem Dan <rotemdan@gmail.com>
2024-10-25 19:15:23 +0300

    Fix signal channel to allow multiple different signals.

--- src/utilities/SignalChannel.ts
+++ src/utilities/SignalChannel.ts
@@ -1,22 +1,29 @@
 export class SignalChannel {
 	channel: MessageChannel
+	handlers = new Map<string, SignalChannelHandler>()
 
 	constructor() {
 		this.channel = new MessageChannel()
 
+		this.channel.port2.onmessage = (event: MessageEvent) => {
+			const signalName = event.data.signalName
+			const signalData = event.data.data
+
+			const handler = this.handlers.get(signalName)
+
+			if (handler) {
+				handler(signalData)
+			}
+		}
 	}
 
-	on(signalName: string, handler: (data?: any) => void) {
-		this.channel.port2.onmessage = (event: MessageEvent) => {
-			if (event.data.signalName != signalName) {
-				return
-			}
-
-			handler(event.data.data)
-		}
+	on(signalName: string, handler: SignalChannelHandler) {
+		this.handlers.set(signalName, handler)
 	}
 
 	send(signalName: string, data?: any) {
 		this.channel.port1.postMessage({ signalName, data })
 	}
 }
+
+export type SignalChannelHandler = (data?: any) => void
