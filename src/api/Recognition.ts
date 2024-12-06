a6240b806aba1975ba72fe10aebe2ca14a1564ab
Rotem Dan <rotemdan@gmail.com>
2024-12-06 07:23:54 +0200

    Pass full, rather than short language code to Amazon Transcribe.

--- src/api/Recognition.ts
+++ src/api/Recognition.ts
@@ -253,7 +253,7 @@ export async function recognize(input: AudioSourceParam, options: RecognitionOpt
 
 			logger.end();
 
-			({ transcript, timeline } = await AmazonTranscribeSTT.recgonize(sourceRawAudio, shortLanguageCode, region, accessKeyId, secretAccessKey))
+			({ transcript, timeline } = await AmazonTranscribeSTT.recgonize(sourceRawAudio, languageCode, region, accessKeyId, secretAccessKey))
 
 			break
 		}
