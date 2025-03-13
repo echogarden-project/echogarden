7a8de87074e6d965e72f76349ddf000abcd11a4a
entermask <48334744+entermask@users.noreply.github.com>
2025-03-13 15:00:53 +0700

    change to new Google TTS AudioEncoding config

--- src/synthesis/GoogleCloudTTS.ts
+++ src/synthesis/GoogleCloudTTS.ts
@@ -1,124 +1,131 @@
-import { request } from 'gaxios'
-import { Logger } from '../utilities/Logger.js'
-import { logToStderr } from '../utilities/Utilities.js'
-import { decodeBase64 } from '../encodings/Base64.js'
+import { request } from "gaxios";
+import { Logger } from "../utilities/Logger.js";
+import { logToStderr } from "../utilities/Utilities.js";
+import { decodeBase64 } from "../encodings/Base64.js";
 
-const log = logToStderr
+const log = logToStderr;
 
 export async function synthesize(
-	text: string,
-	apiKey: string,
-	languageCode = 'en-US',
-	voice = 'en-US-Wavenet-C',
-	speakingRate = 1.0,
-	pitchDeltaSemitones = 0.0,
-	volumeGainDecibels = 0.0,
-	ssml = false,
-	audioEncoding: AudioEncoding = 'MP3_64_KBPS',
-	sampleRate = 24000) {
+  text: string,
+  apiKey: string,
+  languageCode = "en-US",
+  voice = "en-US-Wavenet-C",
+  speakingRate = 1.0,
+  pitchDeltaSemitones = 0.0,
+  volumeGainDecibels = 0.0,
+  ssml = false,
+  audioEncoding: AudioEncoding = "MP3",
+  sampleRate = 24000
+) {
+  const logger = new Logger();
+  logger.start("Request synthesis from Google Cloud");
 
-	const logger = new Logger()
-	logger.start('Request synthesis from Google Cloud')
+  const requestBody = {
+    input: {
+      text: undefined as string | undefined,
+      ssml: undefined as string | undefined,
+    },
 
-	const requestBody = {
-		input: {
-			text: undefined as (string | undefined),
-			ssml: undefined as (string | undefined)
-		},
+    voice: {
+      languageCode,
+      name: voice,
+    },
 
-		voice: {
-			languageCode,
-			name: voice
-		},
+    audioConfig: {
+      audioEncoding,
+      speakingRate,
+      pitch: pitchDeltaSemitones,
+      volumeGainDb: volumeGainDecibels,
+      sampleRateHertz: sampleRate,
+    },
 
-		audioConfig: {
-			audioEncoding,
-			speakingRate,
-			pitch: pitchDeltaSemitones,
-			volumeGainDb: volumeGainDecibels,
-			sampleRateHertz: sampleRate
-		},
+    enableTimePointing: ["SSML_MARK"],
+  };
 
-		enableTimePointing: ['SSML_MARK']
-	}
+  if (ssml) {
+    requestBody.input.ssml = text;
+  } else {
+    requestBody.input.text = text;
+  }
 
-	if (ssml) {
-		requestBody.input.ssml = text
-	} else {
-		requestBody.input.text = text
-	}
+  const response = await request<any>({
+    method: "POST",
 
-	const response = await request<any>({
-		method: 'POST',
+    url: `https://texttospeech.googleapis.com/v1beta1/text:synthesize`,
 
-		url: `https://texttospeech.googleapis.com/v1beta1/text:synthesize`,
+    params: {
+      key: apiKey,
+    },
 
-		params: {
-			'key': apiKey
-		},
+    headers: {
+      "User-Agent": "",
+    },
 
-		headers: {
-			'User-Agent': ''
-		},
+    data: requestBody,
 
-		data: requestBody,
+    responseType: "json",
+  });
 
-		responseType: 'json'
-	})
+  logger.start("Parse result");
 
-	logger.start('Parse result')
+  const result = parseResponseBody(response.data);
 
-	const result = parseResponseBody(response.data)
+  logger.end();
 
-	logger.end()
-
-	return result
+  return result;
 }
 
 function parseResponseBody(responseBody: any) {
-	const audioData = decodeBase64(responseBody.audioContent)
-	const timepoints: timePoint[] = responseBody.timepoints
+  const audioData = decodeBase64(responseBody.audioContent);
+  const timepoints: timePoint[] = responseBody.timepoints;
 
-	return { audioData, timepoints }
+  return { audioData, timepoints };
 }
 
 // Voices with audio samples: https://cloud.google.com/text-to-speech/docs/voices
 export async function getVoiceList(apiKey: string) {
-	const requestURL = `https://texttospeech.googleapis.com/v1beta1/voices`
+  const requestURL = `https://texttospeech.googleapis.com/v1beta1/voices`;
 
-	const response = await request<any>({
-		method: 'GET',
+  const response = await request<any>({
+    method: "GET",
 
-		url: requestURL,
+    url: requestURL,
 
-		params: {
-			'key': apiKey
-		},
+    params: {
+      key: apiKey,
+    },
 
-		headers: {
-			'User-Agent': ''
-		},
+    headers: {
+      "User-Agent": "",
+    },
 
-		responseType: 'json'
-	})
+    responseType: "json",
+  });
 
-	const responseData = response.data
+  const responseData = response.data;
 
-	const voices: GoogleCloudVoice[] = responseData.voices
+  const voices: GoogleCloudVoice[] = responseData.voices;
 
-	return voices
+  return voices;
 }
 
 export type GoogleCloudVoice = {
-	name: string
-	languageCodes: string[]
-	ssmlGender: 'MALE' | 'FEMALE'
-	naturalSampleRateHertz: number
-}
+  name: string;
+  languageCodes: string[];
+  ssmlGender: "MALE" | "FEMALE";
+  naturalSampleRateHertz: number;
+};
 
-export type AudioEncoding = 'LINEAR16' | 'MP3' | 'MP3_64_KBPS' | 'OGG_OPUS' | 'MULAW' | 'ALAW'
+export type AudioEncoding =
+  | "AUDIO_ENCODING_UNSPECIFIED"
+  | "LINEAR16"
+  | "MP3"
+  | "OGG_OPUS"
+  | "MULAW"
+  | "ALAW"
+  | "PCM";
 
 export type timePoint = {
-	markName: string,
-	timeSeconds: number
-}
+  markName: string;
+  timeSeconds: number;
+};
